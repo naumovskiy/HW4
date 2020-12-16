9750cbe59e240d8ac3cb9d30103221b5d81829ad
naumovskiy <59152131+naumovskiy@users.noreply.github.com>
2020-12-16 19:07:10 +0700

    Add files via upload

--- HW4/myapp/main.py
+++ HW4/myapp/main.py
@@ -1,5 +1,4 @@
 from classifier import Classifier
-from codecs import open
 import time
 from flask import Flask, render_template, request
 app = Flask(__name__)
@@ -11,27 +10,14 @@ print("Classifier is successfully loaded")
 print(time.time() - start_time, "seconds")
 
 
-@app.route("/news-demo", methods = ["POST", "GET"])
+@app.route("/", methods = ["POST", "GET"])
 def index_page(text = "", prediction_message = ""):
     if request.method == "POST":
         text = request.form["text"]
 
-        logfile = open("news_demo_logs.txt", "ab", "utf-8")
-
-        # print(text)
-        logfile.write(text)
-        logfile.write("<response>")
-
         prediction_message = classifier.get_result_message(text)
-
-        # print(prediction_message)
-        logfile.write(prediction_message)
-        logfile.write("<response>")
-
-        logfile.close()
 
     return render_template('simple_page.html', text = text, prediction_message = prediction_message)
 
-
 if __name__ == "__main__":
-    app.run(host = '127.0.0.1', port = 80, debug = True)
+    app.run(host = '127.0.0.1', port = 8080, debug = True)
