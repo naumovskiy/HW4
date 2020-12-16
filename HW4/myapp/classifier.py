003f6760b3af7d437188db18f46e022091c03a87
naumovskiy <59152131+naumovskiy@users.noreply.github.com>
2020-12-16 23:04:29 +0700

    Add files via upload

--- HW4/myapp/classifier.py
+++ HW4/myapp/classifier.py
@@ -1,15 +1,16 @@
 import joblib
+import pandas as pd
 
 class Classifier(object):
     def __init__(self):
         self.vectorizer = joblib.load("movie_vectorizer_dump.pkl")
         self.model = joblib.load("movie_model_dump.pkl")
         self.mlb = joblib.load("movie_mlb_dump.pkl")
-        self.treshold_df = joblib.load("movie_treshold_df_dump.pkl")
+        self.treshold_df = pd.read_csv('treshold_df.csv',index_col='index')
 
     def get_name_by_label(self, label):
         try:
-            return self.target_names[label]
+            return  label
         except:
             return "label error"
 
@@ -18,14 +19,15 @@ class Classifier(object):
             vectorized = self.vectorizer.transform([text])
             preds = self.model.predict_proba(vectorized)
             for n, yi in enumerate(['action', 'adventure', 'animation', 'biography', 'comedy', 'crime','drama', 'family', 'fantasy', 'history', 'horror', 'music', 'musical','mystery', 'romance', 'sci-fi', 'sport', 'thriller', 'war', 'western']):
-                preds[:, n] = preds[:, n] > treshold_df.loc[yi, 'best_treshold']
+                preds[:, n] = preds[:, n] > self.treshold_df.loc[yi, 'best_treshold']
             if preds.sum() == 0:
                 preds[6] = 1
-            return ' '.join(mlb.inverse_transform(preds)[0])
+            return ' '.join(self.mlb.inverse_transform(preds)[0])
         except:
             print("prediction error")
             return None 
 
     def get_result_message(self, text):
         prediction = self.predict_text(text)
+        print(prediction)
         return self.get_name_by_label(prediction)
